73e29f8a20375d841971856e34c91b8f1f56af2b
Frank van Wijk <fvanwijk@xebia.com>
2020-01-15 00:24:52 +0100

    Add all available info to DB every 5 minutes

---
+++ src/api.js
@@ -0,0 +1,18 @@
+import admin from 'firebase-admin';
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+
+const db = admin.firestore();
+const observations = db.collection('observations');
+
+export async function addObservation(data) {
+  try {
+    const date = new Date(data.utctime * 1000);
+    return await observations.doc(date.toISOString()).create(data);
+  } catch (e) {
+    console.log(e);
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import fetch from 'node-fetch';
+import { addObservation } from './api.js';
 
+const interval = 5 * 60 * 1000;
 const url = 'http://192.168.2.115/wflexp.json';
+console.log(`Polling ${url} every ${interval / 1000} secs`);
+
 async function getData() {
-  console.log(`Polling ${url}`);
   const data = await (await fetch(url)).json();
-  //console.log(data)
+  await addObservation(data);
 }
 
-setInterval(getData, 1000);
+setInterval(getData, interval);

3f32a13b93e8642962eef389ca5f5268990c3825
Frank van Wijk <fvanwijk@xebia.com>
2020-02-02 10:15:07 +0100

    Add more logging to Cloud Function for debugging

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const { Logging } = require('@google-cloud/logging');
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault()
@@ -13,11 +14,29 @@ function addObservation(data) {
   return observations.doc(date.toISOString()).create(data);
 }
 
+const METADATA = {
+  resource: {
+    type: 'cloud_function',
+    labels: {
+      function_name: 'storeObservation',
+      region: 'europe-west1'
+    }
+  }
+};
+
+const logging = new Logging();
+const log = logging.log('wifilogger');
+
 exports.storeObservation = functions.region('europe-west1').https.onRequest(async (req, res) => {
+  console.log('Trying to log observation');
   try {
     await addObservation(req.body);
     res.json(req.body);
   } catch (e) {
-    res.status(500).send(e);
+    console.error('Could not store observation (console.error)');
+    log.write(
+      log.entry(METADATA, { event: 'store', value: req.body, message: 'Error storing observation (logger api)' })
+    );
+    res.status(500).send('Something went wrong');
   }
 });
